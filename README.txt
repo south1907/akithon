e42a7a15b53a027494932899265b9f5947256e44
Heva <heva@gmail.com>
2022-05-17 00:18:18 +0700

    info to upload package

---
+++ README.txt
@@ -0,0 +1,23 @@
+# Akithon
+## _Akinator client package python_
+
+Base on https://github.com/jgoralcz/aki-api
+
+## Installation
+
+```sh
+pip3 install akithon
+```
+
+## Package
+
+```sh
+https://pypi.org/project/akithon
+```
+
+
+## License
+
+MIT
+
+**Heva**
--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='akithon',
-    url='',
+    url='https://github.com/south1907/akithon',
     author='Heva',
-    author_email='',
-    packages=['akithon'],
-    install_requires=[],
-    version='0.1',
+    author_email='namph.soict@gmail.com',
+    python_requires='>=3.7',
+    version='0.1.1',
     license='MIT',
     description='Akinator client package python',
+    packages=find_packages(exclude=[
+        'akithon_*', 'tests*'
+    ]),
+    keywords='akinator api client library package chatbot magic'
 )
